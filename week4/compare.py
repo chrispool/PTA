832300cce035e540a11d02956a2dfb5c5118927f
Chris Pool <chrispool@gmail.com>
2015-05-15 12:47:21 +0200

    added adjudication

--- week4/compare.py
+++ week4/compare.py
@@ -71,7 +71,61 @@ class Compare():
 		print("ConfusionMatrix")
 		print(ConfusionMatrix(judge1, judge2))
 
+	
+	def adjudication(self):
+		JUDGE1NAME = 'leonardo'
+		JUDGE2NAME = 'chris'
+		rootdir = 'data'
+		for subdir, dirs, files in os.walk(rootdir):
+			newFile = []
+			judge1 = []
+			judge2 = []
+			for filename in files:
+			
+				#Leonardo
+				if filename.endswith(JUDGE1NAME):
+					with open(os.path.join(subdir, filename)) as f:	
+						lines = f.read().splitlines()
+						for line in lines:
+								judge1.append(line.split())
+				#chris
+				if filename.endswith(JUDGE2NAME):
+					with open(os.path.join(subdir, filename)) as f:
+						
+						lines = f.read().splitlines()
+						for line in lines:
+								judge2.append(line.split())
 
+
+			#bekijken of lijsten hetzelfde zijn
+			for i, row in enumerate(judge1):
+				#als beide judges het niks vinden
+				if len(judge1[i]) == 5 and len(judge2[i]) == 5:
+					newFile.append(judge1[i])
+				
+				elif len(judge1[i]) > 6 and len(judge2[i]) > 6:
+					if judge1[i][5] == judge2[i][5] and judge1[i][6] == judge2[i][6]:
+						newFile.append(judge1[i])
+
+				else:
+					print("Judge1 zegt: {} |||||| Judge2 zegt: {}".format(" ".join(judge1[i][3:])," ".join(judge2[i][3:])))
+					inp = input("Judge 1 or 2? ")
+					if inp == 1:
+						newFile.append(judge1[i])
+					else:
+						newFile.append(judge2[i])
+			
+			#write new file
+			f = open(os.path.join(subdir, 'en.tok.off.pos.ent'), 'a')
+			for row in newFile:
+				newRow = "{} \n".format(" ".join(row))
+				f.write(newRow)
+			f.close()
+			print()
+			print("saved file {}".format(subdir +'/en.tok.off.pos.ent'))
+			print()
+		print("Generated Gold standard")
 
 C = Compare()
 C.compareJudges()
+C.adjudication()
